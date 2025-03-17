067daf9833c3c97a751792271ad45fddc4235c8d
SamrudhNelli <samrudhnelli@gmail.com>
2025-03-18 01:02:02 +0530

    Added support for opacity changes in the grid
    The opacity of an individual grid box increases by 10% each time on the
    action of mouse hover or mouseclick.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function func(e)
+{
+    let t = e.style.opacity;
+    if(t != 1)
+    {
+        console.log(t);
+        e.style.opacity = String(parseFloat(t) + 0.1);
+        console.log(e.style.opacity);
+    }
+    else if(e.style.backgroundColor == 'white')
+    {
+        e.style.backgroundColor = 'black';
+        e.style.opacity = '0.1';
+    }
+}
+
 const body_container = document.querySelector('.body_container');
 body_container.style.width = screen.availWidth + 'px';
 body_container.style.height = (screen.availHeight - 25) + 'px';
@@ -22,12 +38,15 @@ for(let i = 0; i < rows; i++)
     {
         grids[i][j] = document.createElement('div');
         grids[i][j].style.width = '40px';
-        grids[i][j].style.border = '2px solid black';
+        grids[i][j].style.border = '1px solid black';
         grids[i][j].style.margin = '0px';
         grids[i][j].style.height = '40px';
         grids[i][j].style.backgroundColor = 'white';
+        grids[i][j].style.opacity = '1.0';
         grids[i][j].style.flexDirection = 'column';
         grids[i].append(grids[i][j]);
+        grids[i][j].addEventListener('mouseover', function() { func(grids[i][j]); });
+        grids[i][j].addEventListener('mouseup', function() { func(grids[i][j]); });
     }
     grid.append(grids[i]);
 }
